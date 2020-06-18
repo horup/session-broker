eef4bdd1c7070f44fcce3b0feb18979004c0f512
horup <sah@sah.dk>
2020-06-18 14:33:27 +0200

    added subscribeCreateSession

--- masterserver/redis/index.ts
+++ masterserver/redis/index.ts
@@ -5,6 +5,7 @@ import { RedisClient } from 'redis';
 import {redis, subscriber} from './redis';
 
 export * from './connect';
+export * from './session';
 
 /**Creates a new id */
 export async function newId() 
@@ -12,7 +13,6 @@ export async function newId()
     const newId = await redis.incr("nextid");
     return newId;
 }
-
 
 export function subscribeDisconnect(f:(clientId:number)=>any)
 {
@@ -41,49 +41,8 @@ export async function publishJoin(join:Join)
     const res = await redis.publish("join", JSON.stringify(join));
 }
 
-export interface SessionAccept
-{
-    clientId:number;
-    sessionid:number;
-    name:string;
-    owner:number;
-}
 
-export async function publishSessionAccept(sessionAccept:SessionAccept)
-{
 
-}
-
-export function subscribeSessionAccept(f:(sessionAccept:SessionAccept)=>any)
-{
-    subscriber.subscribe("session:accept");
-    subscriber.on('message', (channel, value)=>{
-        if (channel == "session:accept")
-            f(JSON.parse(value));
-    });
-}
-
-export interface Session 
-{
-    id:number;
-    name:string;
-    password:string;
-    owner:number;
-}
-
-export function subscribeNewSession(f:(session:Session)=>any)
-{
-    subscriber.subscribe("session:new");
-    subscriber.on('message', (channel, value)=>{
-        if (channel == 'session:new')
-            f(JSON.parse(value));
-    });
-}
-
-export async function publishNewSession(session:Session)
-{
-    const res = await redis.publish("session:new", JSON.stringify(session));
-}
 
 
 export interface Join
---
+++ masterserver/redis/session.ts
@@ -0,0 +1,47 @@
+import { subscriber, redis } from "./redis";
+
+export interface SessionAccept
+{
+    clientId:number;
+    sessionid:number;
+    name:string;
+    owner:number;
+}
+
+export function subscribeSessionAccept(f:(sessionAccept:SessionAccept)=>any)
+{
+    subscriber.subscribe("session:accept");
+    subscriber.on('message', (channel, value)=>{
+        if (channel == "session:accept")
+            f(JSON.parse(value));
+    });
+}
+
+export async function publishSessionAccept(sessionAccept:SessionAccept)
+{
+    const res = await redis.publish("connect", JSON.stringify(sessionAccept));
+}
+
+
+
+
+export interface CreateSession 
+{
+    name:string;
+    password:string;
+    owner:number;
+}
+
+export function subscribeCreateSession(f:(session:CreateSession)=>any)
+{
+    subscriber.subscribe("session:new");
+    subscriber.on('message', (channel, value)=>{
+        if (channel == 'session:new')
+            f(JSON.parse(value));
+    });
+}
+
+export async function publishCreateSession(session:CreateSession)
+{
+    const res = await redis.publish("session:new", JSON.stringify(session));
+}
